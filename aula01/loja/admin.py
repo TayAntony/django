886c2ed2d5fa72af6c1f0c15a56da452a19a3e44
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-02-03 16:54:38 -0300

    relação entre duas tabelas usando ForeignKey

--- aula01/loja/admin.py
+++ aula01/loja/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+admin.site.register(models.Categorias)
 @admin.register(models.Produtos)
+
 class ProdutoAdmin (admin.ModelAdmin):
-    list_display = ['nome', 'preco', 'qtd_estoque']
+    list_display = ['nome', 'preco', 'qtd_estoque', 'validade', 'disponibilidade']
     search_fields = ['nome']
 
 admin.site.register(models.Clientes)
--- aula01/loja/models.py
+++ aula01/loja/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+
+class Categorias(models.Model):
+    nome_modelo = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.nome_modelo
+
 class Produtos (models.Model):
     nome = models.CharField(max_length=255)
     preco = models.DecimalField(max_digits=6, decimal_places=2)
     qtd_estoque = models.IntegerField()
+    descricao = models.TextField()
+    validade = models.DateField()
+    disponibilidade = models.BooleanField()
+
+    categoria_prod = models.ForeignKey(Categorias, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
