ca6ed5cb09024b792df6f363cd9141246a120690
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-02-09 16:56:09 -0300

    atualização
    criação de clientes com várias funções de list e actions, defs para aumentar preço e zerar estoque

--- aula01/loja/admin.py
+++ aula01/loja/admin.py
@@ -1,12 +1,34 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from . import models
-# Register your models here.
+
 
 admin.site.register(models.Categorias)
 @admin.register(models.Produtos)
 
-class ProdutoAdmin (admin.ModelAdmin):
-    list_display = ['nome', 'preco', 'qtd_estoque', 'validade', 'disponibilidade']
-    search_fields = ['nome']
 
-admin.site.register(models.Clientes)
+class ProdutoAdmin (admin.ModelAdmin):
+   
+    list_display = ['nome', 'preco', 'qtd_estoque', 'validade', 'disponibilidade']
+    actions = ['zerar_estoque']
+
+    @admin.action(description='Preço aumentado em 30%%')
+    def aumentar_preco(self, request, queryset):
+        for produto in queryset:
+            produto_atualizado = produto.
+            
+        self.message_user(request, f'{produto_atualizado} produtos foram atualizados!', messages.SUCCESS)
+
+    @admin.action(description='Zerar estoque')
+    def zerar_estoque(self, request, queryset):
+        print(queryset)
+        total_atualizado = queryset.update(qtd_estoque=0)
+        self.message_user(request, f'{total_atualizado} produtos foram atualizados!', messages.WARNING)
+    
+
+
+@admin.register(models.Clientes)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'tipoCliente']
+    list_editable = ['nome', 'tipoCliente']
+    list_filter = ['tipoCliente']
+    
--- aula01/loja/models.py
+++ aula01/loja/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
-
 class Categorias(models.Model):
     nome_modelo = models.CharField(max_length=255, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Categorias'
+
     def __str__(self) -> str:
         return self.nome_modelo
+    
 
 class Produtos (models.Model):
     nome = models.CharField(max_length=255)
@@ -17,12 +20,33 @@ class Produtos (models.Model):
 
     categoria_prod = models.ForeignKey(Categorias, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = 'Produtos'
+
     def __str__(self):
         return self.nome
 
 class Clientes(models.Model):
+    CLIENTE_FREE = 'F'
+    CLIENTE_PREMIUM = 'P'
+    CLIENTE_MASTER = 'M'
+
+    TIPOS_CLIENTES = [
+        ('F', 'Free'),
+        ('P', 'Premium'),
+        ('M', 'Master')
+    ]
     nome= models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    celular = models.CharField(max_length=14)
+    cpf = models.CharField(max_length=14)
     resumo = models.TextField()
-    
+    dataNascimento = models.DateField()
+    dataCadastro = models.DateField(auto_now=True)
+    tipoCliente = models.CharField(max_length=1, choices=TIPOS_CLIENTES, default='F')
+
+    class Meta:
+        verbose_name_plural = 'Clientes'
+
     def __str__(self) -> str:
         return self.nome
