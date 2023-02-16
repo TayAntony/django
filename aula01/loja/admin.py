50baf32310462db595ac7b1041bd68e9aa890414
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-02-16 15:20:21 -0300

    criando filtro baseado no estoque usando SimpleListFilter

--- aula01/loja/admin.py
+++ aula01/loja/admin.py
@@ -1,15 +1,46 @@
 from django.contrib import admin, messages
 from . import models
-
+from django.contrib.admin import ModelAdmin, SimpleListFilter
 
 admin.site.register(models.Categorias)
+
+class EstoqueFiltro(admin.SimpleListFilter):
+    title = "Filter"
+    parameter_name = "avaliar_estoque"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("Estoque zerado", "Estoque zerado"),
+            ("Estoque baixo", "Estoque baixo"),
+            ("Estoque OK", "Estoque OK"),
+            ("Estoque alto", "Estoque alto")
+
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "Estoque zerado":
+            return queryset.filter(
+                qtd_estoque=0
+            )
+        if self.value() == "Estoque baixo":
+            return queryset.filter(
+                qtd_estoque__gte=1,
+                qtd_estoque__lte=10,
+            )
+        if self.value() == "Estoque OK":
+            return queryset.filter(
+                qtd_estoque__gte=11,
+                qtd_estoque__lte=30)
+        if self.value() == "Estoque alto":
+            return queryset.filter(
+                qtd_estoque__gte=30
+            )
+        
 @admin.register(models.Produtos)
-
-
 class ProdutoAdmin (admin.ModelAdmin):
     list_display = ['nome', 'preco', 'qtd_estoque', 'validade', 'disponibilidade', 'avaliar_estoque']
     actions = ['zerar_estoque', 'aumentar_preco', 'diminuir_preco']
-    list_filter = ['qtd_estoque', 'disponibilidade']
+    list_filter = ['disponibilidade', EstoqueFiltro]
     search_fields = ['nome__istartswith']
     list_editable = ['qtd_estoque']
     
@@ -39,22 +70,23 @@ class ProdutoAdmin (admin.ModelAdmin):
         for produto in queryset:
             preco_antigo = produto.preco
             preco_atual = float(preco_antigo) * 0.3
-            preco_atual -= float(preco_antigo)
+            preco_atual = float(preco_antigo) - preco_atual
             total_atualizado = queryset.update(preco=preco_atual)
             print(total_atualizado)
 
         self.message_user(request, f'{total_atualizado} produtos foram atualizados!', messages.SUCCESS)
 
+    @admin.display(ordering='qtd')
     def avaliar_estoque(self, produto):
         if produto.qtd_estoque < 0:
             produto.qtd_estoque = 0
             produto.save()
+        elif produto.qtd_estoque > 30:
+            return 'Estoque alto'
         elif produto.qtd_estoque >=10:
             return 'Estoque OK'
         elif produto.qtd_estoque == 0:
             return 'Estoque zerado'
-        elif produto.qtd_estoque > 30:
-            return 'Estoque alto'
         else:
             return 'Estoque baixo'
 
