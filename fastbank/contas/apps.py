1f1235f818730412d45ce911df9ed18d3c20ecc6
viktormarinho <viktormpcs@gmail.com>
2023-03-18 21:22:33 -0300

    arrumar erro de chave estrangeira faltando on_delete

--- fastbank/contas/apps.py
+++ fastbank/contas/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TransacaoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transacao'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "contas"
