b85cb88a024ede986495bf1c76f1dee4210b021a
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-04-25 16:56:44 -0300

    tentando criar o serializer

--- fastbank/contas/serializer.py
+++ fastbank/contas/serializer.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import *
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = ['nome_cliente', 'contatos_cliente', 'tipo_cliente', 'cpf_cnpj', 'data_nascimento_criacao']
--- fastbank/contas/urls.py
+++ fastbank/contas/urls.py
@@ -1,1 +1,2 @@
 from django.urls import path
+
