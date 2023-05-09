ff591059d169df016c037ef8c45fcb965fff947e
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-05-09 15:49:18 -0300

    testando

--- fastbank/contas/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class ClienteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cliente
-        fields = ['nome_cliente', 'contatos_cliente', 'tipo_cliente', 'cpf_cnpj', 'data_nascimento_criacao']
